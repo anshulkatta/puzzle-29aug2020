4091993ea41132227d0b3f13b8997ac3a628904b
avin77 <shobhit.7@gmail.com>
2020-08-29 16:33:03 +0530

    init

---
+++ Assets/Scripts/SeriesLevelSwitcher.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class SeriesLevelSwitcher : MonoBehaviour
+{
+    // switch scene if button is clicked
+    Button nextSceneChangeButton;
+    void Start()
+    {
+        nextSceneChangeButton = GetComponent<Button>();
+        nextSceneChangeButton.onClick.AddListener(delegate {
+            SceneManager.LoadScene("SeriesScene");
+        });
+    }
+
+}
